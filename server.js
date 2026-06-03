f2059dd07895f2e699545667b313c6fcab0266f7
gilbertababono2-creator <gilbertababono2@gmail.com>
2026-06-03 06:26:31 +0000

    Fix syntax error in CORS configuration

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "*",,
+  origin: "*",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"],
 }));
