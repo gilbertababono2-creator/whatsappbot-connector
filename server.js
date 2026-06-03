cb562182b697da146cf43b0b9844f26a33010959
gilbertababono2-creator <gilbertababono2@gmail.com>
2026-06-03 06:11:43 +0000

    Fix CORS origin setting in server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "*",
+  origin: "*",,
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"],
 }));
